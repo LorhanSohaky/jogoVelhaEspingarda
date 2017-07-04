5e09d7ddd4bad9d9d8be43bf3090570de9ab70ae
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-07-03 22:35:28 -0300

    Inicio da modelagem e testes com o CGI

--- src/main.c
+++ src/main.c
@@ -2,11 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct { unsigned int linha, coluna; } coordenadas;
+#define max 9
+#define lin 3
+#define col 3
+
+typedef struct {
+    unsigned int linha;
+    unsigned int coluna;
+} coordenadas;
+
+typedef struct {
+    unsigned int jogador;
+    coordenadas ponto;
+} jogada;
 
 bool esta_vazio( int *tabuleiro, coordenadas posicao );
 
-void print_jogo( int *tabuleiro ) {
+void print_tabuleiro( int *tabuleiro ) {
     printf( "<p> %d | %d | %d </p>", tabuleiro[0], tabuleiro[1], tabuleiro[2] );
     printf( "<p> -------------</p>" );
     printf( "<p> %d | %d | %d </p>", tabuleiro[3], tabuleiro[4], tabuleiro[5] );
@@ -14,33 +26,43 @@ void print_jogo( int *tabuleiro ) {
     printf( "<p> %d | %d | %d </p>", tabuleiro[6], tabuleiro[7], tabuleiro[8] );
 }
 
-void print_widget( int *tabuleiro ) {
-    printf( "<input type=\"text\" name=\"teste\" value=\"" );
+void print_jogadas( int *tabuleiro ) {
+    printf( "<input type=\"hidden\" name=\"teste\" value=\"" );
     int i, j;
-    for( i = 0; i < 3; i++ ) {
-        for( j = 0; j < 3; j++ ) {
-            printf( "%d %d %d;", tabuleiro[3 * i + j], i, j );
+    for( i = 0; i < lin; i++ ) {
+        for( j = 0; j < col; j++ ) {
+            printf( "%d %d %d;", tabuleiro[lin * i + j], i, j );
         }
     }
 
     printf( "\">" );
 }
 
+void print_jogo( int *tabuleiro ) {
+    print_tabuleiro( tabuleiro );
+    print_jogadas( tabuleiro );
+}
+
+void print_form( int *tabuleiro ) {
+    printf( "<form method=\"GET\" action=\"main\">" );
+    print_jogadas( tabuleiro );
+    printf( "Coordenada x: <input type=\"number\" name=\"x\"><br>" );
+    printf( "Coordenada y: <input type=\"number\" name=\"y\"><br>" );
+    printf( "<input type=\"submit\" value=\"Enviar\">" );
+}
+
 int main() {
     char *data;
-    int *tabuleiro = calloc( 9, sizeof( int ) );
-    float temperatura;
+
+    int *tabuleiro = calloc( lin * col, sizeof( int ) );
     printf( "%s%c%c\n", "Content-Type:text/html;charset=utf-8", 13, 10 );
     printf( "<TITLE>Jogo da velha</TITLE>\n" );
 
     data = getenv( "QUERY_STRING" );
 
-    if( sscanf( data, "temperatura=%f", &temperatura ) != 1 ) {
-        printf( "<P>Error! Invalid data. Data must be numeric." );
-    } else {
-        printf( "<P>Temperatura em Celsius %f para Fahrenheit %f</P>",
-                temperatura,
-                temperatura * 18000 + 32.00 );
+    if( data ) {
+        printf( "<p>veio</p>" );
+        printf( "<p>%s</p>", data );
     }
 
     coordenadas posicao;
@@ -50,16 +72,25 @@ int main() {
     int k = 1;
     for( int i = 0; i < 3; i++ ) {
         for( int j = 0; j < 3; j++ ) {
-            tabuleiro[3 * i + j] = k++;
+            tabuleiro[3 * i + j] = 0;
         }
     }
 
-    print_widget( tabuleiro );
     print_jogo( tabuleiro );
+    print_form( tabuleiro );
 
     return 0;
 }
 
+void ler_modo_jogo() {
+}
+
 bool esta_vazio( int *tabuleiro, coordenadas posicao ) {
     return tabuleiro[3 * posicao.linha + posicao.coluna];
+}
+
+bool init( int *tabuleiro ) {
+    tabuleiro = calloc( max, sizeof( int ) );
+
+    return tabuleiro;
 }
