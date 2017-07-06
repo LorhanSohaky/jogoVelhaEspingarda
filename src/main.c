7148a2c388aa1d430312014a4487b620e7c92123
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-07-06 16:37:41 -0300

    Reorganização das funções e mudança para o método POST

--- src/main.c
+++ src/main.c
@@ -6,6 +6,8 @@
 #define lin 3
 #define col 3
 
+enum { P1 = 1, P2 = 2 };
+
 typedef struct {
     unsigned int linha;
     unsigned int coluna;
@@ -26,39 +28,41 @@ void print_tabuleiro( int *tabuleiro ) {
     printf( "<p> %d | %d | %d </p>", tabuleiro[6], tabuleiro[7], tabuleiro[8] );
 }
 
-void print_jogadas( int *tabuleiro ) {
-    printf( "<input type=\"hidden\" name=\"teste\" value=\"" );
-    int i, j;
-    for( i = 0; i < lin; i++ ) {
-        for( j = 0; j < col; j++ ) {
-            printf( "%d %d %d;", tabuleiro[lin * i + j], i, j );
-        }
-    }
-
-    printf( "\">" );
-}
-
 void print_jogo( int *tabuleiro ) {
     print_tabuleiro( tabuleiro );
-    print_jogadas( tabuleiro );
 }
 
 void print_form( int *tabuleiro ) {
-    printf( "<form method=\"GET\" action=\"main\">" );
-    print_jogadas( tabuleiro );
+    printf( "<form method=\"POST\" action=\"main\">" );
     printf( "Coordenada x: <input type=\"number\" name=\"x\"><br>" );
     printf( "Coordenada y: <input type=\"number\" name=\"y\"><br>" );
     printf( "<input type=\"submit\" value=\"Enviar\">" );
+
+    printf( "<input type=\"hidden\" name=\"jogadas\" value=\"" );
+    int i, j;
+    for( i = 0; i < lin; i++ ) {
+        for( j = 0; j < col; j++ ) {
+            printf( "%d_%d_%d&", tabuleiro[lin * i + j], i, j );
+        }
+    }
+    printf( "\">" );
 }
 
 int main() {
     char *data;
+    char vet[300];
+
+    unsigned int tamanho;
 
     int *tabuleiro = calloc( lin * col, sizeof( int ) );
     printf( "%s%c%c\n", "Content-Type:text/html;charset=utf-8", 13, 10 );
     printf( "<TITLE>Jogo da velha</TITLE>\n" );
 
     data = getenv( "QUERY_STRING" );
+    sscanf( data, "%d", &tamanho );
+    fgets( vet, tamanho + 1, stdin );
+
+    printf( "<p>Stdin:%s</p>\n", vet );
 
     if( data ) {
         printf( "<p>veio</p>" );
