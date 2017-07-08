c7a9b841171078eea3662a216fa2111980f73082
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-07-08 20:15:03 -0300

    Validação do Codacy

--- src/script.js
+++ src/script.js
@@ -1,4 +1,3 @@
 if(document.getElementById("modo").value=="0"){
-    modo=prompt("Modo:\n[1] - PVP\n[2] - PC",1);
-    document.getElementById("modo").value=modo;
+    document.getElementById("modo").value=prompt("Modo:\n[1] - PVP\n[2] - PC",1);
 }
