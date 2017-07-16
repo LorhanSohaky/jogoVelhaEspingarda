bf3ba2ce022db007d7071b2aae1424289a08a3db
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-07-15 22:54:18 -0300

    Correção

--- js/script.js
+++ js/script.js
@@ -6,27 +6,21 @@ function setXY(x,y) {
 
 
 var getUrlParameter = function getUrlParameter(sParam) {
-     var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-         sURLVariables = sPageURL.split('&'),
-         sParameterName,
-         i;
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),sURLVariables = sPageURL.split("&"),sParameterName,i;
 
-     for (i = 0; i < sURLVariables.length; i++) {
-         sParameterName = sURLVariables[i].split('=');
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split("=");
 
-         if (sParameterName[0] === sParam) {
-             return sParameterName[1] === undefined ? true : sParameterName[1];
-         }
-     }
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === "" ? true : sParameterName[1];
+        }
+    }
  };
 
 function getModo(){
-
-     if(document.getElementById("modo").value=="0"){
- 	       document.getElementById("modo").value=getUrlParameter('modo');
-           /*modo=prompt("Modo:\n[1] - PVP\n[2] - PC",1);
-           document.getElementById("modo").value=modo;*/
-       }
+    if(document.getElementById("modo").value==="0"){
+        document.getElementById("modo").value=getUrlParameter("modo");
+    }
 }
 
 
