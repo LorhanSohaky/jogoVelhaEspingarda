8e9b6f1155073d5ddf3ad8ebb6da1ba386b21a63
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-08-24 15:44:52 -0300

    Adição dos headers

--- cgi/prints.h
+++ cgi/prints.h
@@ -1,6 +1,9 @@
 #ifndef PRINTS_H
 #define PRINTS_H
 
+#include "datatype.h"
+#include <stdio.h>
+
 void print_tabuleiro( const int *tabuleiro );
 void print_jogo( const dados *dado, const int *tabuleiro );
 void print_form( const dados *dado );
