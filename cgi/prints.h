f5112f9dddc4e3177b77d0c0f75631d141362500
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-08-24 15:25:04 -0300

    Header das funções de prints

---
+++ cgi/prints.h
@@ -0,0 +1,9 @@
+#ifndef PRINTS_H
+#define PRINTS_H
+
+void print_tabuleiro( const int *tabuleiro );
+void print_jogo( const dados *dado, const int *tabuleiro );
+void print_form( const dados *dado );
+void print_resultado( const dados *dado, const int *tabuleiro );
+
+#endif
